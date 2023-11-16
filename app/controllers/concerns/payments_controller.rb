16a46a09b4800442905a2650cbc4a07a0043143f
Kuldeep Chotaliya <kuldeep.chotaliya@bacancy.com>
2023-11-16 14:40:33 +0530

    setup stripe payment gateway

---
+++ app/controllers/concerns/payments_controller.rb
@@ -0,0 +1,27 @@
+class PaymentsController < ApplicationController
+  def new; end
+
+  def create_session
+    session = Stripe::Checkout::Session.create({
+      customer_email: 'customer@example.com',
+      line_items: [
+        {
+          price_data: {
+            currency: 'inr',
+            product_data: { name: 'Iphone 15 pro' },
+            unit_amount: 80_000 * 100
+          },
+          quantity: 1
+        }
+      ],
+      mode: 'payment',
+      success_url: "#{ENV['DOMAIN']}/success",
+      cancel_url: "#{ENV['DOMAIN']}/cancel"
+    })
+    redirect_to session.url, allow_other_host: true
+  end
+
+  def success; end
+
+  def cancel; end
+end
