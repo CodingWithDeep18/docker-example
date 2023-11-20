a050a68d369c0bb7f32680517b591e1bfa42291b
Kuldeep Chotaliya <kuldeep.chotaliya@bacancy.com>
2023-11-20 13:25:36 +0530

    remove flash message

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -1,6 +1,9 @@
 // Import and register all your controllers from the importmap under controllers/*
 
 import { application } from "./application"
-import HelloController from "./hello_controller"
+import HelloController from "./hello_controller.js"
+import RemovalsController from "./removals_controller.js"
+
 
 application.register("hello", HelloController)
+application.register("removals", RemovalsController)
