f058a678a1397a471ccaf2c583efb67474a86e5b
Kuldeep Chotaliya <kuldeep.chotaliya@bacancy.com>
2023-11-23 11:23:31 +0530

    Added order, shipping_address and line_items model

--- db/schema.rb
+++ db/schema.rb
@@ -10,16 +10,50 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 20_231_120_080_325) do
+ActiveRecord::Schema[7.1].define(version: 2023_11_22_104501) do
   # These are extensions that must be enabled in order to support this database
-  enable_extension 'plpgsql'
+  enable_extension "plpgsql"
 
-  create_table 'products', force: :cascade do |t|
-    t.string 'name'
-    t.decimal 'price'
-    t.text 'description'
-    t.integer 'qty'
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
+  create_table "line_items", force: :cascade do |t|
+    t.bigint "product_id", null: false
+    t.integer "quantity"
+    t.bigint "order_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["order_id"], name: "index_line_items_on_order_id"
+    t.index ["product_id"], name: "index_line_items_on_product_id"
   end
+
+  create_table "orders", force: :cascade do |t|
+    t.string "total_amount"
+    t.integer "status"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "products", force: :cascade do |t|
+    t.string "name"
+    t.decimal "price"
+    t.text "description"
+    t.integer "qty"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "shipping_addresses", force: :cascade do |t|
+    t.string "city"
+    t.string "country"
+    t.string "line1"
+    t.string "line2"
+    t.string "postal_code"
+    t.string "state"
+    t.bigint "order_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["order_id"], name: "index_shipping_addresses_on_order_id"
+  end
+
+  add_foreign_key "line_items", "orders"
+  add_foreign_key "line_items", "products"
+  add_foreign_key "shipping_addresses", "orders"
 end
