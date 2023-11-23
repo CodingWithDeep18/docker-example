0ea253352fcf40d7049f945b2f16b996d69219c9
Kuldeep Chotaliya <kuldeep.chotaliya@bacancy.com>
2023-11-23 16:27:11 +0530

    Changes in flash message

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -3,8 +3,6 @@
 import { application } from "./application"
 import RequestController from "./request_controller.js"
 import HelloController from "./hello_controller.js"
-import RemovalsController from "./removals_controller.js"
 
 application.register("hello", HelloController)
-application.register("removals", RemovalsController)
 application.register("request", RequestController)
