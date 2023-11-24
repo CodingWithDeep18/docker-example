2b26cba71a942d29902c5d9a3607b2db5b988e52
Kuldeep Chotaliya <kuldeep.chotaliya@bacancy.com>
2023-11-24 12:27:41 +0530

    rubocop changes

--- app/models/product.rb
+++ app/models/product.rb
@@ -4,11 +4,11 @@ class Product < ApplicationRecord
   validates :description, presence: true
   validates :qty, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
 
-  def self.ransackable_attributes(auth_object = nil)
-    ["created_at", "description", "id", "id_value", "name", "price", "qty", "updated_at"]
+  def self.ransackable_attributes(_auth_object = nil)
+    %w[created_at description id id_value name price qty updated_at]
   end
 
-  def self.ransackable_associations(auth_object = nil)
+  def self.ransackable_associations(_auth_object = nil)
     []
   end
 end
