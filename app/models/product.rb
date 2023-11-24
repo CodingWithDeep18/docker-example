3d758e9ae77f631b0d37ae65cf42fa0c1f44939b
Kuldeep Chotaliya <kuldeep.chotaliya@bacancy.com>
2023-11-24 12:26:30 +0530

    Search using product name and description

--- app/models/product.rb
+++ app/models/product.rb
@@ -3,4 +3,12 @@ class Product < ApplicationRecord
   validates :price, presence: true, numericality: { only_integer: false, greater_than_or_equal_to: 0 }
   validates :description, presence: true
   validates :qty, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+
+  def self.ransackable_attributes(auth_object = nil)
+    ["created_at", "description", "id", "id_value", "name", "price", "qty", "updated_at"]
+  end
+
+  def self.ransackable_associations(auth_object = nil)
+    []
+  end
 end
