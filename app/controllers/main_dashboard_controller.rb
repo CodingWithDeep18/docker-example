c73d535f1da1046499326c67f1ea15c66f5f97a6
Kuldeep Chotaliya <kuldeep.chotaliya@bacancy.com>
2023-11-27 13:20:38 +0530

    Changes devise generated views

--- app/controllers/main_dashboard_controller.rb
+++ app/controllers/main_dashboard_controller.rb
@@ -1,3 +1,5 @@
 class MainDashboardController < ApplicationController
+  skip_before_action :authenticate_customer!
+
   def index; end
 end
--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -42,7 +42,7 @@ class ProductsController < ApplicationController
     return redirect_to request.referer, notice: 'No file added' if params[:file].nil?
     return redirect_to request.referer, notice: 'Only CSV files allowed' unless params[:file].content_type == 'text/csv'
 
-    CsvImportService.new.call(params[:file])
+    CsvImportService.call(params[:file])
 
     redirect_to request.referer, notice: 'Import started..! Products will imported soon!'
   end
